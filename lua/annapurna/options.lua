b12cad63d73bee4e9e04dfd4ed3f3822a8a8058b
Marco Kaufmann <me@marcokaufmann.ch>
2025-05-26 20:48:56 +0200

    chore: register typ filetype

--- lua/annapurna/options.lua
+++ lua/annapurna/options.lua
@@ -43,3 +43,17 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.opt.relativenumber = false
   end
 })
+
+vim.api.nvim_create_autocmd(
+  {
+    "BufNewFile",
+    "BufRead",
+  },
+  {
+    pattern = "*.typ",
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      vim.api.nvim_buf_set_option(buf, "filetype", "typst")
+    end
+  }
+)
